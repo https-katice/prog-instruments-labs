21b475db7ba46acc45b9645dd2a401ae33cd4be6
https-katice <164325850+https-katice@users.noreply.github.com>
2025-12-13 18:35:27 +0400

    fix logic of main and some regs

--- lab_3/checksum.py
+++ lab_3/checksum.py
@@ -2,19 +2,35 @@ import json
 import hashlib
 from typing import List
 
+"""
+В этом модуле обитают функции, необходимые для автоматизированной проверки результатов ваших трудов.
+"""
+
 
 def calculate_checksum(row_numbers: List[int]) -> str:
+    """
+    Вычисляет md5 хеш от списка целочисленных значений.
+
+    ВНИМАНИЕ, ВАЖНО! Чтобы сумма получилась корректной, считать, что первая строка с данными csv-файла имеет номер 0
+    Другими словами: В исходном csv 1я строка - заголовки столбцов, 2я и остальные - данные.
+    Соответственно, считаем что у 2 строки файла номер 0, у 3й - номер 1 и так далее.
+
+    Надеюсь, я расписал это максимально подробно.
+    Хотя что-то мне подсказывает, что обязательно найдется человек, у которого с этим возникнут проблемы.
+    Которому я отвечу, что все написано в докстринге ¯\_(ツ)_/¯
+
+    :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
+    :return: md5 хеш для проверки через github action
+    """
     row_numbers.sort()
     return hashlib.md5(json.dumps(row_numbers).encode('utf-8')).hexdigest()
 
 
-def serialize_result(variant: int, checksum: str) -> None:
-    result = {
-        "variant": variant,
-        "checksum": checksum
-    }
-    with open('result.json', 'w', encoding='utf-8') as f:
-        json.dump(result, f, indent=2)
+def serialize_result(variant: int, checksum: str, path: str) -> None:
+
+    result = {'variant': variant, 'checksum': checksum}
+    with open(path, 'w') as file:
+        json.dump(result, file, indent=2)
 
 
 if __name__ == "__main__":
