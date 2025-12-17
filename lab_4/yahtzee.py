94194a7cf6cf9b038f6f050299e1d9b6b3189db5
https-katice <164325850+https-katice@users.noreply.github.com>
2025-12-18 00:30:05 +0400

    eliminated duplication of counting logic
    applied the extract method refactoring using make_counts and refactored score_pair, two_pair, three_of_a_kind, four_of_a_kind methods

--- lab_4/yahtzee.py
+++ lab_4/yahtzee.py
@@ -69,68 +69,45 @@ class Yahtzee:
 
     def sixes(self):
         return self.sum_for_number(6)
-    
-    @staticmethod
-    def score_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        at = 0
-        for at in range(6):
-            if (counts[6-at-1] == 2):
-                return (6-at)*2
-        return 0
-    
-    @staticmethod
-    def two_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        n = 0
-        score = 0
-        for i in range(6):
-            if (counts[6-i-1] == 2):
-                n = n+1
-                score += (6-i)
-                    
-        if (n == 2):
-            return score * 2
-        else:
-            return 0
-    
-    @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] == 4):
-                return (i+1) * 4
-        return 0
-    
 
     @staticmethod
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] == 3):
-                return (i+1) * 3
+    def score_pair(d1, d2, d3, d4, d5):
+        dice = [d1, d2, d3, d4, d5]
+        counts = Yahtzee.make_counts(dice)
+        for index in range(5, -1, -1):
+            if counts[index] == 2:
+                return (index + 1) * 2
         return 0
-    
+
+    @staticmethod
+    def two_pair(d1, d2, d3, d4, d5):
+        dice = [d1, d2, d3, d4, d5]
+        counts = Yahtzee.make_counts(dice)
+        score = 0
+        pairs_found = 0
+        for index in range(5, -1, -1):
+            if counts[index] == 2:
+                pairs_found += 1
+                score += (index + 1) * 2
+        return score if pairs_found == 2 else 0
+
+    @staticmethod
+    def three_of_a_kind(d1, d2, d3, d4, d5):
+        dice = [d1, d2, d3, d4, d5]
+        counts = Yahtzee.make_counts(dice)
+        for index in range(6):
+            if counts[index] == 3:
+                return (index + 1) * 3
+        return 0
+
+    @staticmethod
+    def four_of_a_kind(d1, d2, d3, d4, d5):
+        dice = [d1, d2, d3, d4, d5]
+        counts = Yahtzee.make_counts(dice)
+        for index in range(6):
+            if counts[index] == 4:
+                return (index + 1) * 4
+        return 0
 
     @staticmethod
     def smallStraight( d1,  d2,  d3,  d4,  d5):
